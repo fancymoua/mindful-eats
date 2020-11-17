3ab9f82259a477bd7f3b06bc83154b61983ff2be
Fancy <fancymoua@gmail.com>
2020-11-17 10:29:32 -0800

    setup new UI detail view

--- Mindful Eats/Controller/CheckInDetailVC.swift
+++ Mindful Eats/Controller/CheckInDetailVC.swift
@@ -7,6 +7,11 @@ class CheckInDetailVC: UIViewController {
     @IBOutlet weak var foodsLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var emotionsLabel: UILabel!
+    @IBOutlet weak var emotionsView: UIView!
+    @IBOutlet weak var foodsView: UIView!
+    @IBOutlet weak var peopleView: UIView!
+    @IBOutlet weak var placeView: UIView!
+    
     
     var date = Date()
     var foods = String()
@@ -23,6 +28,9 @@ class CheckInDetailVC: UIViewController {
         displayDateTime()
         print(people)
         print(place)
+        
+        customizeNavBar()
+        customizeViews()
     }
     
     func displayDateTime() {
@@ -39,4 +47,31 @@ class CheckInDetailVC: UIViewController {
     
 
     
+}
+
+extension CheckInDetailVC {
+    func customizeNavBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.view.backgroundColor = .clear
+    }
+    
+    func customizeViews() {
+        emotionsView.layer.cornerRadius = emotionsView.frame.height / 14
+        emotionsView.layer.masksToBounds = true
+        emotionsView.layer.borderColor = UIColor.lightGray.cgColor
+        emotionsView.layer.borderWidth = 1.5
+        
+        foodsView.layer.cornerRadius = emotionsView.frame.height / 14
+        foodsView.layer.masksToBounds = true
+        foodsView.layer.borderColor = UIColor.lightGray.cgColor
+        foodsView.layer.borderWidth = 1.5
+        
+        peopleView.layer.borderColor = UIColor.lightGray.cgColor
+        peopleView.layer.borderWidth = 1.2
+        
+        placeView.layer.borderColor = UIColor.lightGray.cgColor
+        placeView.layer.borderWidth = 1.2
+    }
 }
