f92a7b89160f9a4fdf1b9464d7b0b396b9e3a88a
Fancy <fancymoua@gmail.com>
2020-11-17 17:45:27 -0800

    checking for nil on peoplePlace view + presenting alert if nil

--- Mindful Eats/Controller/PeoplePlaceVC.swift
+++ Mindful Eats/Controller/PeoplePlaceVC.swift
@@ -8,40 +8,63 @@ class PeoplePlaceVC: UIViewController {
         super.viewDidLoad()
 
         loadBackgroundImage()
-    }
-    
-    var BGImage = UIImageView()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    var locationSelected = String()
-    var companionSelected = String()
-    
-    @IBAction func locationPressed(_ sender: UITapGestureRecognizer) {
-        locationSelected = sender.name!
         print(locationSelected)
     }
     
+    var BGImage = UIImageView()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var locationSelected: String?
+    var companionSelected: String?
+    var locationBool = false
+    var peopleBool = false
+    
+    @IBAction func locationPressed(_ sender: UITapGestureRecognizer) {
+        if locationBool == false {
+            sender.view?.backgroundColor = .green
+            locationSelected = sender.name!
+            locationBool = true
+        } else if locationBool == true {
+            sender.view?.backgroundColor = .white
+            locationSelected = nil
+            locationBool = false
+        }
+    }
+    
     @IBAction func companionButtonPressed(_ sender: UITapGestureRecognizer) {
-        
-        companionSelected = sender.name!
-        print(companionSelected)
+        if peopleBool == false {
+            sender.view?.backgroundColor = .green
+            companionSelected = sender.name!
+            peopleBool = true
+        } else if locationBool == true {
+            sender.view?.backgroundColor = .white
+            companionSelected = nil
+            peopleBool = false
+        }
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
         
-        let newCheckIn = CheckIn(context: context)
+        if companionSelected == nil || locationSelected == nil {
+            
+            print("Required field missing")
+            let alertController = UIAlertController(title: "Please answer both questions.", message: "", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "Okay", style: .cancel, handler: nil)
+            alertController.addAction(confirmAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            let newCheckIn = CheckIn(context: context)
+            
+            let destVC = storyboard?.instantiateViewController(identifier: "AddFoodsView") as! AddFoodVC
+            
+            newCheckIn.location = locationSelected
+            newCheckIn.companion = companionSelected
         
-        let destVC = storyboard?.instantiateViewController(identifier: "AddFoodsView") as! AddFoodVC
-        
-        newCheckIn.location = locationSelected
-        newCheckIn.companion = companionSelected
-    
-        destVC.newCheckIn = newCheckIn
-        
-        show(destVC, sender: self)
+            destVC.newCheckIn = newCheckIn
+            
+            show(destVC, sender: self)
+        }
     }
-    
 }
 
 extension PeoplePlaceVC {
